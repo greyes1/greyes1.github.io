9b3408bc6c342689832c0193189aee8eb2a69926
greyes1 <thatgreyes@gmail.com>
2019-08-20 19:28:28 -0500

    added material and Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+  	BrowserAnimationsModule,
+  	MatCardModule,
     BrowserModule,
     AppRoutingModule
   ],
